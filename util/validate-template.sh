dffbf2f5c2fe6ab1d916768f9bbaf65140da6729
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2018-12-26 02:31:53 -0200

    Fix: using env var.

--- util/validate-template.sh
+++ util/validate-template.sh
@@ -5,17 +5,15 @@
 #
 # 2018-10     ferreira.mv
 
-file="${1}"
-
-[ "${1}" == "" ] && {
+[ "${file}" == "" ] && {
   echo
-  echo "  Usage: $0  filename.ext"
+  echo "  Usage: file=filename $0"
   echo
   exit 1
 }
 
-echo "=="
-echo "== File: [${file}]"
+echo "--"
+echo "-- File: [${file}]"
 echo
 
 aws cloudformation validate-template \
