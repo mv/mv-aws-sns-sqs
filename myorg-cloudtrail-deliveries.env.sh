b22819871f0621fc311d937ab12cd42e208b9935
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2018-12-26 04:02:11 -0200

    New: config sets.

---
+++ myorg-cloudtrail-deliveries.env.sh
@@ -0,0 +1,12 @@
+#
+# Usage:
+#   source ./file.env.sh
+#
+# 2018-12     ferreira.mv
+
+export file=sns-sqs.template.yaml
+export stack=sns-sqs-myorg-cloudtrail-deliveries
+export param=myorg-cloudtrail-deliveries.param.json
+export force=n
+
+
---
+++ myorg-notifications.env.sh
@@ -0,0 +1,12 @@
+#
+# Usage:
+#   source ./file.env.sh
+#
+# 2018-12     ferreira.mv
+
+export file=sns-sqs.template.yaml
+export stack=sns-sqs-myorg-notifications
+export param=myorg-notifications.param.json
+export force=n
+
+
